133238e0448d8a1454a14fe39387af20c58525dd
tuminqiang <2637191946@qq.com>
2019-11-28 23:42:43 +0800

    项目开头

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <vheader>头部组件</vheader>
+    <div>中间组件</div>
+    <div>尾部组件</div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import headerContain from './components/header/headerContain.vue';
+
+  export default {
+    components: {
+      vheader: headerContain
+    }
+  };
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
 </style>
---
+++ src/components/header/headerContain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="headerContain">
+      <h1>头部组件</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'headerContain'
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
